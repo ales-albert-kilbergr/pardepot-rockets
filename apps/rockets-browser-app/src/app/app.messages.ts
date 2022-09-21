ac73220cd206579790fb4943d4e3495d2bdb9326
ales albert KILBERGR <ales.albert@kilbergr.eu>
2022-09-21 21:10:20 +0200

    feat(intl): basic setup

---
+++ apps/rockets-browser-app/src/app/app.messages.ts
@@ -0,0 +1,8 @@
+import { defineMessage } from 'react-intl';
+
+export const APP_INTL_MESSAGES = {
+  heading: defineMessage({
+    description: 'Application heading',
+    defaultMessage: 'Rockets catalog',
+  }),
+};
--- apps/rockets-browser-app/src/app/app.tsx
+++ apps/rockets-browser-app/src/app/app.tsx
@@ -1,9 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { FormattedMessage } from 'react-intl';
+import { APP_INTL_MESSAGES } from './app.messages';
+import { IntlProvider } from 'react-intl';
 
 export type IRocketBorwserAppComponent = React.FC;
 
 export const RocketBrowserApp: IRocketBorwserAppComponent = () => {
-  return <h1>Rockets APP</h1>;
+  return (
+    <IntlProvider messages={{}} locale="en">
+      <h1>
+        <FormattedMessage {...APP_INTL_MESSAGES.heading} />
+      </h1>
+    </IntlProvider>
+  );
 };
 
 RocketBrowserApp.displayName = 'RocketBrowserApp';
